fix normalization in trainer

train.py
@@ -10,11 +10,11 @@ from torchvision.transforms.functional import center_crop, pil_to_tensor
 
 from prior import DiffusionPrior, NoiseScheduler, OpenClipAdapter, PriorTransformer
 
-BATCH_SIZE = 1
-CLIP_DIM = 1280
+BATCH_SIZE = 128
+CLIP_DIM = 512
 CLIP_CONTEXT = 77
 SEED = 1337
-PARAMETERIZATION = "v"
+PARAMETERIZATION = "x0"
 SCALE_IMAGE_EMBEDDING = True
 
 datasets.logging.set_verbosity_error()
@@ -32,7 +32,7 @@ def collate_fn(tokenizer, batch):
     images = [example[0] for example in batch]
 
     # center crop to 224x224
-    images = [pil_to_tensor(center_crop(image, (224, 224))) for image in images]
+    images = [pil_to_tensor(center_crop(image, (224, 224))).float() / 255 for image in images]
 
     # stack the images
     images = torch.stack(images)
@@ -45,21 +45,17 @@ def main():
     print("#--- Creating Model ---#")
     prior_xf = PriorTransformer(
         ctx_len=CLIP_CONTEXT,
-        emb_dim=1024,
-        num_layers=16,
-        num_heads=16,
+        emb_dim=768,
+        num_layers=12,
+        num_heads=12,
         final_ln=True,
         clip_dim=CLIP_DIM,
     )
 
-    scheduler = NoiseScheduler(
-        beta_schedule="cosine",
-        timesteps=1000,
-        loss_type="l2"
-    )
+    scheduler = NoiseScheduler(beta_schedule="cosine", timesteps=1000, loss_type="l2")
 
     language_model = OpenClipAdapter(
-        path="hf-hub:laion/CLIP-ViT-bigG-14-laion2B-39B-b160k"
+        path="hf-hub:laion/CLIP-ViT-B-32-laion2B-s34B-b79K"
     )
 
     prior = DiffusionPrior(
@@ -72,8 +68,20 @@ def main():
 
     print("#--- Loading Dataset ---#")
     # dataset = datasets.load_dataset("fusing/wikiart_captions")
-    training_dataset = wds.WebDataset(urls="/home/nousr/data/image/laion_coyo_local/{00000..00048}.tar").shuffle(1000).decode("pil", handler=wds.handlers.warn_and_continue).to_tuple("jpg", "txt")
-    validation_dataset = wds.WebDataset(urls="/home/nousr/data/image/laion_coyo_local/00049.tar").shuffle(1000).decode("pil", handler=wds.handlers.warn_and_continue).to_tuple("jpg", "txt")
+    training_dataset = (
+        wds.WebDataset(
+            urls="/home/nousr/data/image/laion_coyo_local/{00000..00048}.tar"
+        )
+        .shuffle(1000)
+        .decode("pil", handler=wds.handlers.warn_and_continue)
+        .to_tuple("jpg", "txt")
+    )
+    validation_dataset = (
+        wds.WebDataset(urls="/home/nousr/data/image/laion_coyo_local/00049.tar")
+        .shuffle(1000)
+        .decode("pil", handler=wds.handlers.warn_and_continue)
+        .to_tuple("jpg", "txt")
+    )
 
     # set the tokenizer for the dataloader
     collate = partial(collate_fn, language_model.tokenize_text)
@@ -93,8 +101,20 @@ def main():
         pin_memory=False,
     )
 
-    trainer = Trainer(max_steps=-1, precision="bf16-mixed", accumulate_grad_batches=16, val_check_interval=25*16, limit_val_batches=1, logger=False)
-    trainer.fit(model=prior, train_dataloaders=train_dataloader, val_dataloaders=valid_dataloader)
+    trainer = Trainer(
+        max_steps=-1,
+        precision="bf16-mixed",
+        accumulate_grad_batches=1,
+        val_check_interval=1000,
+        limit_val_batches=1,
+        logger=False,
+        enable_checkpointing=False
+    )
+    trainer.fit(
+        model=prior,
+        train_dataloaders=train_dataloader,
+        val_dataloaders=valid_dataloader,
+    )
 
 
 if __name__ == "__main__":