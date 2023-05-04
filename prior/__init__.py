d20687fce5c496895e0dc2a3efcfab688b4a2160
nousr <51308183+nousr@users.noreply.github.com>
2023-05-04 15:58:26 -0700

    add utils to module

--- prior/__init__.py
+++ prior/__init__.py
@@ -2,3 +2,4 @@ from prior.prior_transformer import PriorTransformer
 from prior.diffusion_prior import DiffusionPrior
 from prior.gaussian_diffusion import NoiseScheduler
 from prior.adapter import OpenClipAdapter, BaseClipAdapter
+from prior.utils import instantiate_from_config
