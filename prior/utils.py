7f3034e95facb768efdf3118d2f2f54bd3d668d7
nousr <51308183+nousr@users.noreply.github.com>
2023-05-04 15:08:40 -0700

    move to config-based approach

---
+++ prior/utils.py
@@ -0,0 +1,13 @@
+import importlib
+
+def instantiate_from_config(config):
+    return get_obj_from_str(config["target"])(**config.get("params", dict()))
+
+def get_obj_from_str(string, reload=False, invalidate_cache=True):
+    module, cls = string.rsplit(".", 1)
+    if invalidate_cache:
+        importlib.invalidate_caches()
+    if reload:
+        module_imp = importlib.import_module(module)
+        importlib.reload(module_imp)
+    return getattr(importlib.import_module(module, package=None), cls)
