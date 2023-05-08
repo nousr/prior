592f0d2067b268d864ff6324e1afbe140d688f79
nousr <51308183+nousr@users.noreply.github.com>
2023-05-08 15:27:17 -0700

    add ema functionality

--- prior/__init__.py
+++ prior/__init__.py
@@ -3,3 +3,4 @@ from prior.diffusion_prior import DiffusionPrior
 from prior.gaussian_diffusion import NoiseScheduler
 from prior.adapter import OpenClipAdapter, BaseClipAdapter
 from prior.utils import instantiate_from_config, get_obj_from_str
+from prior.ema import LitEma
