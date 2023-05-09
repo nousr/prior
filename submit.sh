608ad6fb4e291cb11108f97c0f7e3c7c616ed3e8
nousr <51308183+nousr@users.noreply.github.com>
2023-05-09 23:17:30 +0000

    add slurm launch script

---
+++ submit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# SLURM SUBMIT SCRIPT
+#SBATCH --job-name=prior
+#SBATCH --partition=g80
+#SBATCH --nodes=8
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8
+#SBATCH --mem=0
+#SBATCH --account=controlnet
+#SBATCH --output=%x_%j.out
+
+eval "$(conda shell.bash hook)"
+conda activate torch 
+
+module load cuda/11.8
+
+export FI_EFA_FORK_SAFE=1
+export FI_EFA_USE_DEVICE_RDMA=1
+export OMPI_MCA_mtl_base_verbose=1
+export FI_PROVIDER=efa
+export NCCL_TREE_THRESHOLD=0
+
+srun python train.py --config_path=configs/prior_big_g.yaml --num_workers=6 --num_nodes=8 --devices=8
+
